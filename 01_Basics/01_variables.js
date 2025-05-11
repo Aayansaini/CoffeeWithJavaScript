0e1a1829fcdce857a6d7f583e97b3a42d4b75c9f
Aayan saini <110893943+Aayansaini@users.noreply.github.com>
2025-05-11 09:25:22 +0000

    Some Sugar added in coffee

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 22334455;
+let accountEmail = "aayangoogle@gmail.com"
+var accountPassword = "111111"
+accountCity = "jaipur"
+
+let accountStatement;
+
+
+accountEmail = "jecaayan@gmail.com"
+accountPassword = "000000"
+accountCity = "sydny"
+
+console.table([accountId , accountEmail, accountPassword , accountCity, accountStatement])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //define newer version of js
+
+// alert(2+8) we are using in node js not in browser
+
+console.log(2+3)
+
+console.log("AayanSaini")
+
+let name = "Aayan" //String 
+let marks = 40 // NUmber 
+let isloggedout = true //boolean type
+let state;
+
+//BigInt
+//string
+//boolean--> true or false
+//undefined
+//symbol
+
+
+//OBJECT
+
+console.log(typeof "undefined") // undefined
+console.log(typeof "null") // object
+
+
+
+
+
