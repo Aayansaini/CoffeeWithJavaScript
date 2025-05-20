5ed5df77b4b5cd242a4d0b26aecd981dced568e8
Aayan saini <110893943+Aayansaini@users.noreply.github.com>
2025-05-20 06:42:44 +0000

    Math.Nums

---
+++ 01_Basics/06_nums_maths.js
@@ -0,0 +1,43 @@
+const score = 400
+
+const balance = new Number(100)
+
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 222.345
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundereds = 100000
+
+// console.log(hundereds.toLocaleString('en-IN'));
+
+// -----------Maths-------------
+
+// console.log(Math);
+
+// console.log(Math.abs(-9));
+
+// console.log(Math.round(33.66));
+
+// console.log(Math.ceil(33.44));
+
+// console.log(Math.floor(33.55));
+
+// console.log(Math.sqrt(64));
+
+// console.log(Math.min(3,4,5,6,78,22,44,22,33,1,23,44,33));
+
+// console.log(Math.max(3,4,5,6,78,22,44,22,33,1,23,44,33));
+
+console.log(Math.random());
+
+console.log((Math.random()*10) + 1);
+
+const min =10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
